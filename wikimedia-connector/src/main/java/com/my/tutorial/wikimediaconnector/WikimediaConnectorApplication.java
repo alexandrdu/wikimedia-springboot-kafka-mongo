b644bdadfce014b83482416efeb343223948c192
Dubinin Oleksandr <alexandr.dubinin@extern.a1.at>
2023-11-01 15:32:41 +0100

    Fix imports

--- wikimedia-connector/src/main/java/com/my/tutorial/wikimediaconnector/WikimediaConnectorApplication.java
+++ wikimedia-connector/src/main/java/com/my/tutorial/wikimediaconnector/WikimediaConnectorApplication.java
@@ -1,6 +1,5 @@
 package com.my.tutorial.wikimediaconnector;
 
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
